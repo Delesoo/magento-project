35aeefa057d9267a5b999f50480d415d5ee4ad2f
Delesoo <strongtoka@gmail.com>
2024-12-25 12:53:22 +0400

    ko observables

---
+++ src/app/code/Macademy/InventoryFulfillment/view/frontend/web/js/box-configurations.js
@@ -0,0 +1,38 @@
+define([
+    'uiComponent',
+    'ko'
+], function (
+    Component,
+    ko
+) {
+    'use strict';
+
+    const boxConfiguration = () => {
+        return {
+            length: ko.observable(),
+            width: ko.observable(),
+            height: ko.observable(),
+            weight: ko.observable(),
+            unitsPerBox: ko.observable(),
+            numberOfBoxes: ko.observable(),
+        };
+    };
+    return Component.extend({
+        defaults: {
+            boxConfigurations: ko.observableArray([boxConfiguration()])
+        },
+        initialize() {
+            this._super();
+            console.log('The boxConfigurations component has been loaded');
+        },
+        handleAdd() {
+            this.boxConfigurations.push(boxConfiguration());
+        },
+        handleDelete(index) {
+            this.boxConfigurations.splice(index, 1);
+        },
+        handleSubmit() {
+            console.log('Submitted yoo');
+        }
+    });
+});
--- src/app/code/Macademy/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
+++ src/app/code/Macademy/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
@@ -1,19 +1,25 @@
 define([
     'uiComponent',
     'ko',
-    'mage/storage'
+    'mage/storage',
+    'jquery',
+    'mage/translate'
 ], function (
     Component,
     ko,
-    storage
+    storage,
+    $,
+    $t
 ) {
     'use strict';
 
     return Component.extend({
         defaults: {
             template: 'Macademy_InventoryFulfillment/sku-lookup',
+            placeholder: $t('Example: %1').replace('%1', '24-MB01'),
             sku: ko.observable('24-MB01'),
-            messageResponse: ko.observable('')
+            messageResponse: ko.observable(''),
+            isSuccess: ko.observable(false),
         },
         initialize() {
             this._super();
@@ -21,15 +27,22 @@ define([
             console.log('The skuLookup component has been loaded.');
         },
         handleSubmit() {
+            $('body').trigger('processStart');
             this.messageResponse('');
+            this.isSuccess(false);
 
             storage.get(`rest/V1/products/${this.sku()}`)
                 .done(response => {
-                    this.messageResponse(`Product found! <strong>${response.name}</strong>`);
+                    this.messageResponse($t('Product Find! %1').replace('%1', `<strong>${response.name}</strong>`));
+                    this.isSuccess(true);
                 })
                 .fail(() => {
-                    this.messageResponse('Product not found homie, go find it urself.');
+                    this.messageResponse($t('Product not found homie, go find it urself.'));
+                    this.isSuccess(false);
                 })
+                .always(() => {
+                    $('body').trigger('processStop');
+                });
         }
     });
 });
